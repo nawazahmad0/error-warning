50c1362b5a4b21fd86f585163a61e66d5fd918ea
remdil <167093544+remdil45@users.noreply.github.com>
2024-04-29 16:14:58 +0530

    Update cmds

---
+++ scripts/cmds/shizuka.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+async function a(api, event, args, message) {
+  try {
+    const a = args.join(" ").trim();
+
+    if (!a) {
+      return message.reply("ex: {p} cmdName {your question} ");
+    }
+
+    const b = "you're shizuka from doraemon. you are toxic and you hate pathetic losers like nobita. you are also gold digger. you are secretly dating both dekisuki and sunio. you always mock and roast nobita for being loser."; // the more better content you give the  best it became
+    const c = await d(a, b);
+
+    if (c.code === 2 && c.message === "success") {
+      message.reply(c.answer, (r, s) => {
+        global.GoatBot.onReply.set(s.messageID, {
+          commandName: module.exports.config.name,
+          uid: event.senderID 
+        });
+      });
+    } else {
+      message.reply("Please try again later.");
+    }
+  } catch (e) {
+    console.error("Error:", e);
+    message.reply("An error occurred while processing your request.");
+  }
+}
+
+async function d(a, b) {
+  try {
+    const d = await axios.get(`https://personal-ai-phi.vercel.app/kshitiz?prompt=${encodeURIComponent(a)}&content=${encodeURIComponent(b)}`);
+    return d.data;
+  } catch (f) {
+    console.error("Error from api", f.message);
+    throw f;
+  }
+}
+
+module.exports = {
+  config: {
+    name: "shizh",// add your ai name here
+    version: "1.0",
+    author: "luis Lavigne",
+    role: 0,
+    longDescription: "your ai description",// ai description
+    category: "ai",
+    guide: {
+      en: "{p}cmdName [prompt]"// add guide based on your ai name
+    }
+  },
+  
+  handleCommand: a,
+  onStart: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  },
+  onReply: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  }
+};
