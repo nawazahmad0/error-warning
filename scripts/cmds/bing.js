7dd95ce3192b3e952b3f79d3ed35de297b2b9a10
Waring <159174106+nawazahmad0@users.noreply.github.com>
2024-08-13 12:02:33 +0530

    Update bing.js

--- scripts/cmds/bing.js
+++ scripts/cmds/bing.js
@@ -6,16 +6,15 @@ const baseApiUrl = async () => {
   return base.data.api;
 }; 
 
-
 module.exports = {
   config: {
-    name: "dalle",
-    aliases: ["bing","create","imagine"],
+    name: "dalle3",
+    aliases: ["dalle","bing","create"],
     version: "1.0",
     author: "Dipto",
     countDown: 15,
     role: 0,
-    shortDescription: "Generate images powerby by Dalle3",
+    description: "Generate images powerby by Dalle3",
     longDescription: "Generate images by Unofficial Dalle3",
     category: "download",
     guide: {
@@ -29,12 +28,12 @@ module.exports = {
    return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Dipto and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
   }
     try {
-      const fff = "19T1qFhba9dq98ePlVz4CfY3lED_f9U3FC9Opy8ogP1Bktk5zBAHBgdp_gW6OBn_hEgUt_ccmRlNvWCf7dsPAMZ43Nl-tJve2_bqzbjm19tCYn0ZOdod9usycNCVXSAn-Ci6nFvB_tBNTXeJpCdqHT24WNVfi2SV5IQhbVrZ0Y7EV-oXb_dQ7MeB_Gq1ocBoYnNBlJGTixh707wdLHMx2Mg", "19T1qFhba9dq98ePlVz4CfY3lED_f9U3FC9Opy8ogP1Bktk5zBAHBgdp_gW6OBn_hEgUt_ccmRlNvWCf7dsPAMZ43Nl-tJve2_bqzbjm19tCYn0ZOdod9usycNCVXSAn-Ci6nFvB_tBNTXeJpCdqHT24WNVfi2SV5IQhbVrZ0Y7EV-oXb_dQ7MeB_Gq1ocBoYnNBlJGTixh707wdLHMx2Mg"]
-        const col = fff[Math.floor(Math.random() * fff.length)]
+const tl = ["19T1qFhba9dq98ePlVz4CfY3lED_f9U3FC9Opy8ogP1Bktk5zBAHBgdp_gW6OBn_hEgUt_ccmRlNvWCf7dsPAMZ43Nl-tJve2_bqzbjm19tCYn0ZOdod9usycNCVXSAn-Ci6nFvB_tBNTXeJpCdqHT24WNVfi2SV5IQhbVrZ0Y7EV-oXb_dQ7MeB_Gq1ocBoYnNBlJGTixh707wdLHMx2Mg","19T1qFhba9dq98ePlVz4CfY3lED_f9U3FC9Opy8ogP1Bktk5zBAHBgdp_gW6OBn_hEgUt_ccmRlNvWCf7dsPAMZ43Nl-tJve2_bqzbjm19tCYn0ZOdod9usycNCVXSAn-Ci6nFvB_tBNTXeJpCdqHT24WNVfi2SV5IQhbVrZ0Y7EV-oXb_dQ7MeB_Gq1ocBoYnNBlJGTixh707wdLHMx2Mg"];
+const cookies = tl[Math.floor(Math.random() * tl.length)];
       const w = await api.sendMessage("Wait koro baby < 😽", event.threadID);
   
-const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookies=${col}`)
-      var data = response.data.imgUrls;
+const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookie=${cookies}`)
+      const data = response.data.imgUrls;
       if (!data || data.length === 0) {
         api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
       }
@@ -42,22 +41,18 @@ const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&k
       for (let i = 0; i < data.length; i++) {
         const imgUrl = data[i];
         const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'dvassests', `${i + 1}.jpg`);
+        const imgPath = path.join(__dirname, 'cache', `${i + 1}.jpg`);
         await fs.outputFile(imgPath, imgResponse.data);
         diptoo.push(fs.createReadStream(imgPath));
       }
       await api.unsendMessage(w.messageID);
       await api.sendMessage({
-  body: `✅ | Here's Your Generated Photo<😘`,
+  body: `✅ |Naw Baby Tumar Generated Pic<😘`,
         attachment: diptoo
       },event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
       await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
-    } finally {
-      for (let ii; ii < data.length; ii++){
-           fs.unlinkSync(__dirname, 'dvassests', `${i + 1}.jpg`);
-      }
     }
   }
 }
